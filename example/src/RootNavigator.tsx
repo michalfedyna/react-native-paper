c070fcbf117fcb2c9098e5d60a2cfcc18a00fab3
Michał Fedyna <michal@fedyna.pl>
2026-08-06 15:26:43 +0200

    feat: searchbar in the example list

--- example/src/RootNavigator.tsx
+++ example/src/RootNavigator.tsx
@@ -34,10 +34,10 @@ function Header({ navigation, route, options, back }: NativeStackHeaderProps) {
   );
 
   return (
-    <Appbar.Header elevated>
+    <Appbar.Header>
       {back ? backAction : isIOS ? searchAction : null}
       <Appbar.Content title={options.title || route.name} />
-      {!isIOS && searchAction}
+      {!isIOS && !back && searchAction}
       <Appbar.Action icon="cog" onPress={togglePreferences} />
     </Appbar.Header>
   );
@@ -66,6 +66,7 @@ const Root = createNativeStackNavigator({
       screen: ExampleList,
       options: {
         title: 'Examples',
+        headerShown: false,
       },
       linking: '',
     }),
